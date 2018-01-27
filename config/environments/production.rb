43fca919170f800532b7bf41d1b48cfdfbc25648
Jose Galisteo <ceritium@gmail.com>
2018-01-27 14:55:23 +0100

    upgrade-webpacker-react

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # Make javascript_pack_tag lookup digest hash to enable long-term caching
+  config.webpacker.check_yarn_integrity = false  # Make javascript_pack_tag lookup digest hash to enable long-term caching
   config.x.webpacker[:digesting] = true
 
   # Settings specified here will take precedence over those in config/application.rb.
